3a6aec319a8f9082486a70d3a54a0c96e0b70843
johann <johann.espanet@gmail.com>
2020-10-02 10:38:01 +0200

    make:crud

---
+++ src/Form/BienType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Bien;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BienType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('description')
+            ->add('prix')
+            ->add('photo')
+            ->add('categorie')
+            ->add('titre')
+            ->add('type')
+            ->add('createdAt')
+            ->add('updatedAt')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Bien::class,
+        ]);
+    }
+}
